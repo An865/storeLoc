4e59e306edf148f6b51386381291b1bba739a1df
An865 <77985358+An865@users.noreply.github.com>
2021-06-12 16:59:39 -0700

    readFile in server.js updated comment

--- server.js
+++ server.js
@@ -4,15 +4,17 @@ const app = express();
 const fs = require('fs')
 const http = require('http');
 
-module.exports = server = (data) => {fs.readFile('./index.html', function (err, html) {
-   if (err) {
-       throw err; 
-   }       
-   http.createServer((request, response) => {  
-       response.writeHeader(200, {"Content-Type": "text/html"});  
-       response.write(html);  
-       response.end();  
-   }).listen(8080);
-});
+//fs.readFile(filename, encoding, callback(error, data)) where data is contents of the file
+module.exports = server = (data) => {
+fs.readFile('./index.html', function (err, html) {
+    if (err) {
+        throw err; 
+    }       
+    http.createServer((request, response) => {  
+        response.writeHeader(200, {"Content-Type": "text/html"});  
+        response.write(html);  
+        response.end();  
+    }).listen(8080);
+    });
 console.log('My first Node test server is running on Port 8080.');
 }
