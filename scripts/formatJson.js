7ca2df5500b56a3d246bb24c4e9d9e15fd34aca2
An865 <77985358+An865@users.noreply.github.com>
2021-06-23 13:11:52 -0700

    reformated formatJson file

--- scripts/formatJson.js
+++ scripts/formatJson.js
@@ -1,15 +1,22 @@
 module.exports = formatJson = (storeInformation) => {
     //data categories (top most row of google sheet)
     let dataCategories = storeInformation[0];
-    let stores = {};
+    let stores = [];
 
     //for each row in spreadsheet
     storeInformation.forEach((col) => {
+        //object to store individual store data
+        let indStore = {};
         //for the first (top most) row
         for (let i = 0; i < dataCategories.length; i++) {
             //stores to be returned and serve as input for mapbox
-            //stores.dataCategories[i] = row[i];
-            console.log(dataCategories[i] + ' : ' + col[i]);
+
+            let jsonKey = dataCategories[i];
+            let jsonValue = col[i];
+
+            indStore[jsonKey] = jsonValue;
         }
+        stores.push(indStore);
     });
+    console.log(stores[1]);
 };
